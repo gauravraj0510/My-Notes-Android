caa04e101324203cf115d73ce57335a1cefb454f
gauravraj0510 <62883388+gauravraj0510@users.noreply.github.com>
2020-05-01 13:20:55 +0500

    recycler view added

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -5,13 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -26,10 +34,13 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
 
     private static final String TAG = "MainActivity";
     RecyclerView recyclerView;
+    NotesRecyclerAdapter notesRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +69,32 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
                 .setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        addNote(noteEditText.getText().toString());
                     }
                 })
                 .setNegativeButton("Cancel",null)
                 .show();
+    }
+
+    private void addNote(String text){
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        Note note = new Note(text, false, new Timestamp(new Date()), userId);
+
+        FirebaseFirestore.getInstance().collection("notes")
+                .add(note)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(MainActivity.this, "Note added successfully!", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void startLoginActivity()
@@ -112,24 +144,33 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
     protected void onStop() {
         super.onStop();
         FirebaseAuth.getInstance().removeAuthStateListener(this);
+        if(notesRecyclerAdapter!=null){
+            notesRecyclerAdapter.stopListening();
+        }
     }
 
     @Override
     public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        if(FirebaseAuth.getInstance().getCurrentUser()==null){
+        if(firebaseAuth.getInstance().getCurrentUser()==null){
             startLoginActivity();
             return;
         }
-        else{
-            FirebaseAuth.getInstance().getCurrentUser().getIdToken(true)
-                    .addOnSuccessListener(new OnSuccessListener<GetTokenResult>() {
-                        @Override
-                        public void onSuccess(GetTokenResult getTokenResult) {
-                            Log.d(TAG,"onSuccess: "+getTokenResult.getToken());
-                        }
-                    });
-        }
-        Log.d(TAG,"onAuthStateChanged: "+firebaseAuth.getCurrentUser().getPhoneNumber());
 
+        initRecyclerView(firebaseAuth.getCurrentUser());
     }
+
+    private void initRecyclerView(FirebaseUser user){
+
+        Query query = FirebaseFirestore.getInstance()
+                .collection("notes")
+                .whereEqualTo("userId", user.getUid());
+
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class)
+                .build();
+
+        notesRecyclerAdapter = new NotesRecyclerAdapter(options);
+        recyclerView.setAdapter(notesRecyclerAdapter);
+        notesRecyclerAdapter.startListening();
+     }
 }
